903fed5e12c2db76e35688c2f40a4b09b5a4a4a0
alonzofroman <alonzofroman@gmail.com>
2021-09-26 14:14:18 -0700

    Started CSS styling of page and quizbox

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,18 @@
 
 //Set a user score, starting at zero
 
+//Set at visible timer to create a time limit, ending the quiz when it hits zero
+
 //Have them be displayed as text one at a time
 
 //Take the user's input and check if it matches the predetermined answers
 
 //If correct, add one point to user score
 
+//If incorrect, take off time from the timer
+
 //When answered, move to the next question
 
-//Set at visible timer to create a time limit, ending the quiz when it hits zero
+//Display the user's score and track it locally when the quiz is complete, allowing the user to input their initials
 
-//Display the user's score and track it locally when the quiz is complete
+
