95436b3558815f264e510e5bad93dab251a85fe2
alonzofroman <alonzofroman@gmail.com>
2021-09-26 10:30:08 -0700

    Began html page and pseudocode for js

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+//Write every question on the quiz
+
+//Write every answer
+
+//Set a user score, starting at zero
+
+//Have them be displayed as text one at a time
+
+//Take the user's input and check if it matches the predetermined answers
+
+//If correct, add one point to user score
+
+//When answered, move to the next question
+
+//Set at visible timer to create a time limit, ending the quiz when it hits zero
+
+//Display the user's score and track it locally when the quiz is complete
