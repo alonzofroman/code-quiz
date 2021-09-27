512e9cb1ab57dc780dc6c2749c42910f3560ae3f
alonzofroman <alonzofroman@gmail.com>
2021-09-26 19:58:05 -0700

    Began writing questions, edited quiz box in html to have question and answer containers

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,52 @@
-//Write every question on the quiz
+//Link divs to javascript
+const scoreBox = document.getElementById("scores");
+const startButton = document.getElementById("start-button");
+const countDownTimer = document.getElementById("timer");
 
-//Write every answer
+const quizBox = document.getElementById("quiz");
+const questionSlot = document.getElementById("question");
+const optionASlot = document.getElementById("optionA");
+const optionBSlot = document.getElementById("optionB");
+const optionCSlot = document.getElementById("optionC");
+
+const selectA =document.getElementById("selectA");
+const selectB = document.getElementById("selectB"); //Edit these to add event listeners
+const selectC = document.getElementById("selectC");
+
+//Write every question on the quiz with three answer options
+
+const questionList = [
+   //Question 1
+    {
+        question: "What does var mean in relation to Javascript?",
+            optionA: "Variant",
+            optionB: "Variable",
+            optionC: "Variety",
+            correctAnswer: optionB,
+    },
+    //Question 2
+    {
+        question: "Which tag do you use to link an external Javascript page to HTML?",
+            optionA: "link",
+            optionB: "style",
+            optionC: "script",
+           correctAnswer: optionC,
+    },
+    //Question 3
+]
+
+
+
 
 //Set a user score, starting at zero
+
+var userScore = 0,
 
 //Set at visible timer to create a time limit, ending the quiz when it hits zero
 
 //Have them be displayed as text one at a time
+
+
 
 //Take the user's input and check if it matches the predetermined answers
 
