2981ae1688a1abb4d2c73d8898b5a96a854ab73b
Roman <alonzofroman@gmail.com>
2021-09-27 19:41:16 -0700

    Deleted functions and tried value of radio method

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,17 @@ const countDownTimer = document.getElementById("timer");
 
 const quizBox = document.getElementById("quiz");
 const questionSlot = document.getElementById("question");
-const optionASlot = document.getElementById("optionA");
-const optionBSlot = document.getElementById("optionB");
-const optionCSlot = document.getElementById("optionC");
-const optionDSlot = document.getElementById("optionD");
+const optionASlot = document.getElementById("aSlot");
+const optionBSlot = document.getElementById("bSlot");
+const optionCSlot = document.getElementById("cSlot");
+const optionDSlot = document.getElementById("dSlot");
+
+
+const optionASelect = document.getElementById("optionA");
+const optionBSelect = document.getElementById("optionB");
+const optionCSelect = document.getElementById("optionC");
+const optionDSelect = document.getElementById("optionD");
+const nextButton = document.getElementById("nextButton");
 
 
 
@@ -21,7 +28,8 @@ const questionList = [
             optionA: "Variant",
             optionB: "Variable",
             optionC: "Variety",
-            optionD: "Variance"
+            optionD: "Variance",
+            correctAnswer: "B",
     },
     //Question 2
     {
@@ -30,48 +38,48 @@ const questionList = [
             optionB: "style",
             optionC: "script",
             optionD: "href",
+            correctAnswer: "C",
     },
     //Question 3
 ]
 
 //Set variables for true statements when pressed
-var selectA = optionA.value = false;
-var selectB = optionB.value = false;
-var selectC = optionC.value = false;
-var selectD = optionD.value = false;
-
-function selectAnswer() {
-     selectA = !selectA;  //I'm trying to get this working
- };
-
-optionA.addEventListener("click", selectAnswer());
-
-
-
-console.log(selectA);
 
 
 //Set at visible timer to create a time limit, ending the quiz when it hits zero
 
 
 
+//Have them be displayed as text one at a time
+function displayQuestion() {
+    let questionNumber = 0;
+    let currentQuestion = questionList[questionNumber];  //Set variables to iterate through question list one at a time
+    questionSlot.innerHTML += "<p>" + currentQuestion.question + "</p>";
+    optionASlot.innerHTML += currentQuestion.optionA;
+    optionBSlot.innerHTML += currentQuestion.optionB;
+    optionCSlot.innerHTML += currentQuestion.optionC;
+    optionDSlot.innerHTML += currentQuestion.optionD;
+}
+
+var checkedAnswer = document.querySelector('input[name="option"]:checked'); 
+console.log(checkedAnswer);
+
+// function clickNext() {
+//     if (correctAnswer===
+// }
+
 function startQuiz() {
 
 //Set a user score, starting at zero
 let userScore = 0; 
 
-let questionNumber = 0;
-let currentQuestion = questionList[questionNumber];
+displayQuestion();
 
-//Have them be displayed as text one at a time
 
-questionSlot.innerHTML += "<p>" + currentQuestion.question + "</p>";
-optionASlot.innerHTML += currentQuestion.optionA;
-optionBSlot.innerHTML += currentQuestion.optionB;
-optionCSlot.innerHTML += currentQuestion.optionC;
-optionDSlot.innerHTML += currentQuestion.optionD;
 
 }
+
+
 
 //Take the user's input and check if it matches the predetermined answers
 
@@ -84,4 +92,3 @@ optionDSlot.innerHTML += currentQuestion.optionD;
 //Display the user's score and track it locally when the quiz is complete, allowing the user to input their initials
 
 
-startQuiz();
